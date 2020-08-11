2ac9a3673f7441b5fca27e144e36b98358313dca
datagirl <datagirlcmr@gmail.com>
2020-08-12 00:22:41 +0100

    deploy to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "application#index"
   resources :steps
-  post 'auth/login', to: 'authentication#authenticate'
-  post 'signup', to: 'users#create'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#show'
+  put '/edit-profile', to: 'users#update'
+  post '/auth/login', to: 'authentication#authenticate'
 end
