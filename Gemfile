e0fc6fd4ca10a69d8c28d512ee9e78f59b85cfaf
datagirl <datagirlcmr@gmail.com>
2020-08-13 11:49:19 +0100

    configure rack-cors

--- Gemfile
+++ Gemfile
@@ -17,15 +17,11 @@ gem 'rubocop', '~>0.81.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
-
-
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
-
-
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 gem 'rack-cors'
--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -19,7 +21,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HealthyStepsApi
+module ResellApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
@@ -29,37 +31,9 @@ module HealthyStepsApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    use Rack::Cors do
-      allow do
-        origins 'localhost:3000', '127.0.0.1:3000',
-                /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
-                # regular expressions can be used here
-    
-        resource '/file/list_all/', :headers => 'x-domain-token'
-        resource '/file/at/*',
-            methods: [:get, :post, :delete, :put, :patch, :options, :head],
-            headers: 'x-domain-token',
-            expose: ['Some-Custom-Response-Header'],
-            max_age: 600
-            # headers to expose
-      end
-    
-      allow do
-        origins '*'
-        resource '/public/*', headers: :any, methods: :get
-    
-        # Only allow a request for a specific host
-        resource '/api/v1/*',
-            headers: :any,
-            methods: :get,
-            if: proc { |env| env['HTTP_HOST'] == 'api.example.com' }
-      end
-    end
-
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
-
