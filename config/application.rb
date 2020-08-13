241e4b9d0c5294535f9c4fb3621a0bd35689b5dd
datagirl <datagirlcmr@gmail.com>
2020-08-13 11:42:05 +0100

    install rack cors gem

--- config/application.rb
+++ config/application.rb
@@ -29,9 +29,37 @@ module HealthyStepsApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    use Rack::Cors do
+      allow do
+        origins 'localhost:3000', '127.0.0.1:3000',
+                /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+                # regular expressions can be used here
+    
+        resource '/file/list_all/', :headers => 'x-domain-token'
+        resource '/file/at/*',
+            methods: [:get, :post, :delete, :put, :patch, :options, :head],
+            headers: 'x-domain-token',
+            expose: ['Some-Custom-Response-Header'],
+            max_age: 600
+            # headers to expose
+      end
+    
+      allow do
+        origins '*'
+        resource '/public/*', headers: :any, methods: :get
+    
+        # Only allow a request for a specific host
+        resource '/api/v1/*',
+            headers: :any,
+            methods: :get,
+            if: proc { |env| env['HTTP_HOST'] == 'api.example.com' }
+      end
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
+
